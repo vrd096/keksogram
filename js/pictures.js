5c147b719df35414fcc320aa76f9c824d73c93d4
Evgeniy <vrd096@mail.ru>
2019-06-04 16:48:21 +0500

    Update pictures.js

--- js/pictures.js
+++ js/pictures.js
@@ -1,5 +1,7 @@
 var instaComments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 var instaDescription = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......', 'Вот это тачка!'];
+var pictureTemplate = document.querySelector('#picture-template');
+var instaArray = [];
 
 var randomLike = function (min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -11,26 +13,34 @@ var randomString = function (param) {
  };
 
 var CreateObject = function (value) {
-    var instaObject = {};
-    var instaArray = [];
-    for (var i = 1; i <= value; i++) {
-        
-        instaObject.url = 'photos/' + i + '.jpg';
-        instaObject.likes = randomLike(15, 200);
-        instaObject.comments = randomString(instaComments);
-        instaObject.description = randomString(instaDescription);
-        
-        console.log(instaObject);
-        instaArray.push(instaObject);
-        
-    }
-    // for (var i = 0; i < instaArray.length; i++) {
-    //     instaObject.url = 'photos/' + i + '.jpg';
-    //     instaObject.likes = randomLike(15, 200);
-    //     instaObject.comments = randomString(instaComments);
-    //     instaObject.description = randomString(instaDescription);
-    // }
-    console.log(instaArray); 
    
+
+    for (var i = 1; i <= value; i++) {
+        var instaObject = {
+            url: 'photos/' + i + '.jpg',
+            likes: randomLike(15, 200),
+            comments: randomString(instaComments),
+            description: randomString(instaDescription)
+        };  
+        instaArray.push(instaObject);
+    }
+    
+    return instaArray;
 };
 CreateObject(25);
+
+var makeElement = function (tagName, className, text) {
+    var element = document.createElement(tagName);
+    element.classList.add(className);
+    if (text) {
+      element.textContent = text;
+      };
+      return element;
+    };
+
+var renderCards = function () {
+   
+};
+renderCards();
+
+// console.log(instaArray);
